ceaf745bcc48c95a7c36bd41e9e9be8b043a5462
sunghyun9737 <sunghyun9737@gmail.com>
2025-12-30 10:05:43 +0900

    jsp project start

---
+++ src/main/java/com/sist/web/service/RecipeServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sist.web.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sist.web.mapper.RecipeMapper;
+import com.sist.web.vo.RecipeVO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RecipeServiceImpl implements RecipeService{
+	private final RecipeMapper rMapper;
+	
+	@Override
+	public List<RecipeVO> recipeListData(int start) {
+		// TODO Auto-generated method stub
+		return rMapper.recipeListData(start);
+	}
+	@Override
+	public int recipeTotalPage() {
+		// TODO Auto-generated method stub
+		return rMapper.recipeTotalPage();
+	}
+}
